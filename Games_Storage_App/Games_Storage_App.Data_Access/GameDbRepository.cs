7c389db3a7c2adb6ea6e2db8401012360170b440
Антон Кутовой <antonykutovoi@gmail.com>
2022-01-23 16:54:26 +0800

    add game repository and interface;

---
+++ Games_Storage_App/Games_Storage_App.Data_Access/GameDbRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Games_Storage_App.Data_Access
+{
+    public class GameDbRepository : IGameRepository
+    {
+        private readonly GameContext gameContext;
+
+        public GameDbRepository(GameContext gameContext)
+        {
+            this.gameContext = gameContext;
+        }
+        public Game Get(Guid id)
+        {
+            var allGames = GetAll();
+            return allGames.FirstOrDefault(p => p.Id == id);
+        }
+
+        public List<Game> GetAll()
+        {
+            return gameContext.Games.AsNoTracking().ToList();
+        }
+
+        public void Create(Game game)
+        {
+            gameContext.Games.Add(game);
+            gameContext.SaveChanges();
+        }
+    }
+}
